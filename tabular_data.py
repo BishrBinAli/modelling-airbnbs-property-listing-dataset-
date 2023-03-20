564f37a8487e1fe47a5d1dd1c73b01e29fdc9cb4
BishrBinAli <bishrkc@gmail.com>
2023-03-20 00:31:12 +0000

    Create tabular_data.py with functions to load, clean and save tabular listings data

---
+++ tabular_data.py
@@ -0,0 +1,54 @@
+# %%
+import pandas as pd
+
+
+def remove_rows_with_missing_ratings(df):
+    rating_columns = [col for col in df.columns if col.endswith('_rating')]
+    df = df.dropna(subset=rating_columns, how='all')
+    return df
+
+
+def combine_description_strings(df):
+    # Lists are stored as string in csv file
+    df['Description'] = df['Description'].apply(
+        lambda x: pd.eval(x) if pd.notna(x) else x)
+    df = df.dropna(subset='Description')
+    # Removing empty quotes and first title in list
+    df['Description'] = df['Description'].apply(
+        lambda x: [val for val in x if val != "" and val != "About this space"])
+    # Combining to single string
+    df['Description'] = df['Description'].apply(lambda x: " ".join(x))
+    return df
+
+
+def set_default_feature_values(df):
+    df = df.fillna({'guests': 1, 'beds': 1, 'bathrooms': 1, 'bedrooms': 1})
+    return df
+
+
+def clean_tabular_data(df):
+    df = remove_rows_with_missing_ratings(df)
+    df = combine_description_strings(df)
+    df = set_default_feature_values(df)
+    return df
+
+
+# %%
+if __name__ == "__main__":
+    # %%
+    listings_df = pd.read_csv(
+        './airbnb-property-listings/tabular_data/listing.csv')
+
+    # %%
+    # There is a row with column values from the third column shifted to the right by one resulting in an extra unnamed column
+    shifted_ind = listings_df.index.get_indexer(
+        listings_df[listings_df['Unnamed: 19'].notna()].index)
+    listings_df.iloc[shifted_ind, 3:-1] = listings_df.iloc[shifted_ind, 4:]
+    listings_df = listings_df.drop(columns='Unnamed: 19')
+
+    # %%
+    listings_df = clean_tabular_data(listings_df)
+
+    # %%
+    listings_df.to_csv(
+        './airbnb-property-listings/tabular_data/clean_tabular_data.csv', index=False)
